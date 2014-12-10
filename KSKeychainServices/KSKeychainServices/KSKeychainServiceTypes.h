383734b52bc3a45c347f7a8c1d9f2b93fcaf5024
david <davidlivadaru@gmail.com>
2014-12-10 23:51:09 +0200

    framework-base-setup - add project

---
+++ KSKeychainServices/KSKeychainServices/KSKeychainServiceTypes.h
@@ -0,0 +1,9 @@
+//
+//  KSKeychainServiceTypes.h
+//  KSKeychainServices
+//
+//  Created by david on 10/12/14.
+//  Copyright (c) 2014 David Live Org. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
---
+++ KSKeychainServices/KSKeychainServices/KSKeychainServices.h
@@ -0,0 +1,19 @@
+//
+//  KSKeychainServices.h
+//  KSKeychainServices
+//
+//  Created by david on 10/12/14.
+//  Copyright (c) 2014 David Live Org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KSKeychainServices.
+FOUNDATION_EXPORT double KSKeychainServicesVersionNumber;
+
+//! Project version string for KSKeychainServices.
+FOUNDATION_EXPORT const unsigned char KSKeychainServicesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KSKeychainServices/PublicHeader.h>
+
+
